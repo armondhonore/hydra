3ef017fcb8cf5bf9cec4daa4b35f52a5df2acbcb
Armond Honore <armondhonore@Armonds-MacBook-Air.local>
2026-06-29 19:52:33 -0400

    deploy: hydra bake SECRETS/URLS env into entrypoint (avoid platform var-mangling), build custom image

--- hydra-entrypoint.sh
+++ hydra-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 export DSN="sqlite:///data/hydra.sqlite?_fk=true"
+export SECRETS_SYSTEM="a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4"
+export URLS_SELF_ISSUER="https://relaxed-weasel-hydra.cloud.nexlayer.ai/"
+export OIDC_SUBJECT_IDENTIFIERS_SUPPORTED_TYPES="public"
+export OIDC_SUBJECT_IDENTIFIERS_PAIRWISE_SALT="a1b2c3d4e5f6a1b2"
 until hydra migrate sql -e --yes; do echo "migrate retry..."; sleep 2; done
 exec hydra serve all --dev
